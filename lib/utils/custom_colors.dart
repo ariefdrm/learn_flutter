a1383e4814e0dbbd45c0995ef5ad060241b6780e
ariefdrm <ariefdarmawan5522@gmail.com>
2025-04-29 21:09:08 +0700

    refactor: refactor utils/custom_colors.dart

--- lib/utils/custom_colors.dart
+++ lib/utils/custom_colors.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-
-extension CustomColors on Colors {
+class CustomColors {
   static const Color darkMidnight = Color(0xFF021526);
   static const Color lightMidnight = Color(0xFF0E1E3D);
   static const Color lightSky = Color(0xFF3F8FFF);
